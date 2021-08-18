225816a46c78fcc435be97ece718fcab4d45a34e
beriom <86018772+beriom@users.noreply.github.com>
2021-08-18 10:39:11 -0300

    Create autocommit.bat

---
+++ autocommit.bat
@@ -0,0 +1,3 @@
+git add -u                               REM :: Only add tracked files
+git commit -m "your commit message"      REM :: Commit all your added files
+git push origin master                   REM :: Branch (i.e. master)
