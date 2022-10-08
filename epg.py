7538b692ac490033ead5808e99d0485ae4bc84b8
beriom <86018772+beriom@users.noreply.github.com>
2022-10-08 11:26:00 -0300

    ok

--- epg.py
+++ epg.py
@@ -1,5 +1,10 @@
 import requests
 
+
 url = 'http://m3u4u.com/xml/5g28nepzw9sk2kjyzpe9'
 
-response = requests.get(url)
+
+r = requests.get(url, stream=True, headers={'User-agent': 'Mozilla/5.0'})
+
+with open(r'C:\Users\macelgomesfilho\Desktop\EPG\output/brasil_2.xml', 'wb') as f:
+    f.write(r.content)
